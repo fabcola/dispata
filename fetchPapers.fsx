a76d3b9bc3faf1b292afb9ead0ae60a08f33b7ac
fabrizio <fabrizio.cola2@gmail.com>
2021-12-10 13:38:37 +0800

    Manual version of keyword fetching for a paper

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -4,10 +4,16 @@ open FSharp.Data
 let apiKey = "58e12278a2ab0dc7ca70e0713a1cd2c3"
 
 // Run the HTTP web request
-let response = Http.RequestString( "https://api.elsevier.com/content/metadata/article", httpMethod = "GET",
-    query   = [ "query", "cooling"],
+let response2 = Http.RequestString( "https://api.elsevier.com/content/article/entitlement", httpMethod = "GET",
+    query   = [ "apiKey", apiKey;"doi", "10.1016/j.arabjc.2017.05.011"],
     headers = [ "Accept", "application/json";"X-ELS-APIKey","58e12278a2ab0dc7ca70e0713a1cd2c3"])
 
-let response = Http.RequestString( "https://api.elsevier.com/authenticate", httpMethod = "GET",
-    query   = [ "apiKey", apiKey],
-    headers = [ "Accept", "application/json";"Authorization","Bearer 58e12278a2ab0dc7ca70e0713a1cd2c3"])
+let response = HtmlDocument.Load( "https://www.sciencedirect.com/science/article/pii/S2052297521000883")
+
+let keywords (res:HtmlDocument) = 
+    res.Descendants["div"]
+    |> Seq.filter (fun a -> 
+        a.TryGetAttribute("class")
+        |> Option.map (fun cls -> cls.Value()) = Some "keyword")
+    |> Seq.map(fun x -> x.InnerText().ToLower())
+    |> Seq.toList
