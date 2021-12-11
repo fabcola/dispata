3d03beeacb60a1884e4fd344ecac5b8a4dc082fd
fabrizio <fabrizio.cola2@gmail.com>
2021-12-11 11:21:52 +0800

    Get all kwrds from a journal issue

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -1,29 +1,39 @@
 #r "nuget: FSharp.Data"
 open FSharp.Data
 
-let response = HtmlDocument.Load( "https://www.sciencedirect.com/science/article/pii/S2052297521000883")
-let journalList = HtmlDocument.Load("https://www.sciencedirect.com/journal/applied-energy/vol/307/suppl/C")
+let articlePage (pii:string) = match pii.Length with 
+                                | 17 -> HtmlDocument.Load("https://www.sciencedirect.com/science/article/pii/"+pii)
+                                | _ -> failwith "Incorrect article pii"
+
+let journalIssue = HtmlDocument.Load("https://www.sciencedirect.com/journal/applied-energy/vol/307/suppl/C")
 // Can i create a generic field finder?
-let getKeywords (htmlPage:HtmlDocument) = 
-    htmlPage.Descendants["div"]
+let getArticleKeywords (articlePage:HtmlDocument) = 
+    articlePage.Descendants["div"]
     |> Seq.filter (fun a -> 
         a.TryGetAttribute("class")
         |> Option.map (fun cls -> cls.Value()) = Some "keyword")
     |> Seq.map(fun x -> x.InnerText().ToLower())
-    |> Seq.toList
 
-let getMeta (htmlPage:HtmlDocument) = 
-    htmlPage.Descendants["meta"]
+let getArticleMeta (articlePage:HtmlDocument) = 
+    articlePage.Descendants["meta"]
     |> Seq.map(fun s -> 
         (s.AttributeValue("name"),s.AttributeValue("content")))
-    |> Seq.toList
 
-let getLinks (htmlPage:HtmlDocument) = 
-    htmlPage.Descendants["a"]
+let getJournalIssueLinks (journalIssue:HtmlDocument) = 
+    journalIssue.Descendants["a"]
     |> Seq.map (fun a -> 
         a.AttributeValue("href"))
     |> Seq.filter(fun link -> link.Contains("pii"))
     |> Seq.map(fun s -> 
         s.Split("/")
-        |> )
+        |> Seq.item 4 )
+    |> Seq.distinct
+
+let getJournalIssueKeywords (articleList:HtmlDocument) =
+    getJournalIssueLinks articleList
+    |> Seq.map(fun art ->
+        art
+        |> articlePage
+        |> getArticleKeywords)
+    |> Seq.concat
     |> Seq.toList
