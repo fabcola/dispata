e608c64c15d9942bb19f8859f6660f316cdfd4e3
fabrizio <fabrizio.cola2@gmail.com>
2021-12-11 10:16:40 +0800

    get list of papers

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -2,7 +2,7 @@
 open FSharp.Data
 
 let response = HtmlDocument.Load( "https://www.sciencedirect.com/science/article/pii/S2052297521000883")
-
+let journalList = HtmlDocument.Load("https://www.sciencedirect.com/journal/applied-energy/vol/307/suppl/C")
 // Can i create a generic field finder?
 let getKeywords (htmlPage:HtmlDocument) = 
     htmlPage.Descendants["div"]
@@ -16,4 +16,14 @@ let getMeta (htmlPage:HtmlDocument) =
     htmlPage.Descendants["meta"]
     |> Seq.map(fun s -> 
         (s.AttributeValue("name"),s.AttributeValue("content")))
+    |> Seq.toList
+
+let getLinks (htmlPage:HtmlDocument) = 
+    htmlPage.Descendants["a"]
+    |> Seq.map (fun a -> 
+        a.AttributeValue("href"))
+    |> Seq.filter(fun link -> link.Contains("pii"))
+    |> Seq.map(fun s -> 
+        s.Split("/")
+        |> )
     |> Seq.toList
