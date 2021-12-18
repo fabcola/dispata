bbc3089c9d26a0683fc67a68fe183753f7961875
fabrizio <fabrizio.cola2@gmail.com>
2021-12-18 19:12:32 +0800

    Word embeddings stored in postgre

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -1,6 +1,30 @@
 #r "nuget: FSharp.Data"
-open FSharp.Data
+#r "nuget: Npgsql.FSharp"
 
+open FSharp.Data
+open System.IO
+
+open Npgsql.FSharp
+
+//////////////////// ADD DATA to POSTGRESQL /////////////////////
+let addWord (word:string, vector:int64[]) : int =
+    let connectionString = "Host=localhost; Database=word2vec; Username=postgres; Password=admin;"
+    connectionString
+    |> Sql.connect
+    |> Sql.query "INSERT INTO public.encodings (word, encoder) VALUES (@word, @vector)"
+    |> Sql.parameters [ "@word", Sql.text word; "@vector", Sql.int64Array vector ]
+    |> Sql.executeNonQuery
+
+let convertEmbedding (word:string[]) = 
+    (word.[0], Array.map(fun x -> int64((float x)*1.0e6)) word.[1..])
+
+let wordEmbeddings = File.ReadLines(@"C:\Users\Fabrizio\Downloads\17\model.txt")
+                        |> Seq.map(fun (w:string) -> w.Split(" "))
+                        |> Seq.map(convertEmbedding)
+                        |> Seq.map(addWord) 
+//////////////////////////////////////////////////////////////////
+/// 
+/// 
 let articlePage (pii:string) = match pii.Length with 
                                 | 17 -> HtmlDocument.Load("https://www.sciencedirect.com/science/article/pii/"+pii)
                                 | _ -> failwith "Incorrect article pii"
@@ -37,3 +61,4 @@ let getJournalIssueKeywords (articleList:HtmlDocument) =
         |> getArticleKeywords)
     |> Seq.concat
     |> Seq.toList
+
