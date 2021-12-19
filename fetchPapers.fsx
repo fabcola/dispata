29865ae8609b8a56d5d8f542949652e34d433cfe
fabrizio <fabrizio.cola2@gmail.com>
2021-12-19 16:22:08 +0800

    Distance metrics implemented

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -1,9 +1,9 @@
 #r "nuget: FSharp.Data"
 #r "nuget: Npgsql.FSharp"
-
+#r "nuget: MathNet.Numerics.FSharp"
 open FSharp.Data
 open System.IO
-
+open MathNet.Numerics.LinearAlgebra
 open Npgsql.FSharp
 
 //////////////////// ADD DATA to POSTGRESQL /////////////////////
@@ -18,13 +18,34 @@ let addWord (word:string, vector:int64[]) : int =
 let convertEmbedding (word:string[]) = 
     (word.[0], Array.map(fun x -> int64((float x)*1.0e6)) word.[1..])
 
-let wordEmbeddings = File.ReadLines(@"C:\Users\Fabrizio\Downloads\17\model.txt")
+let wordEmbeddings = File.ReadAllLines(@"C:\Users\Fabrizio\Downloads\17\model.txt")
                         |> Seq.map(fun (w:string) -> w.Split(" "))
                         |> Seq.map(convertEmbedding)
-                        |> Seq.map(addWord) 
+                        |> Seq.toList
+                        |> List.map(addWord) 
+                        
 //////////////////////////////////////////////////////////////////
-/// 
-/// 
+
+let getWordEncoding (word:string) =
+    let connectionString = "Host=localhost; Database=word2vec; Username=postgres; Password=admin;"
+    connectionString
+    |> Sql.connect
+    |> Sql.query (sprintf "SELECT word, encoder FROM public.encodings WHERE word = '%s' LIMIT 1" word)
+    //|> Sql.parameters [ "@target_word", Sql.text word]
+    |> Sql.execute (fun read -> 
+        read.int64Array "encoder")
+ 
+let encoderToArray (encoder:int64[] list) : Vector<float> =
+    match encoder with
+    | [] -> vector [for i in 1..300 -> 0.0]
+    | _ -> vector (Array.map(fun x -> float(x)/1.0e6) (List.item 0 encoder))
+    //|> Array.map(fun x -> float(x)/1.0e6)
+    //|> vector 
+
+let a = encoderToArray (getWordEncoding "cooling");;
+let b = encoderToArray (getWordEncoding "refrigeration");;
+let distance = (a-b).L2Norm() 
+
 let articlePage (pii:string) = match pii.Length with 
                                 | 17 -> HtmlDocument.Load("https://www.sciencedirect.com/science/article/pii/"+pii)
                                 | _ -> failwith "Incorrect article pii"
