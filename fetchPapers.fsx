ce16e982de811e0587ea1f2d9aaa61baa6f9f2eb
fabrizio <fabrizio.cola2@gmail.com>
2022-01-02 23:22:40 +0800

    Clustering done

--- fetchPapers.fsx
+++ fetchPapers.fsx
@@ -1,10 +1,12 @@
 #r "nuget: FSharp.Data"
+#r "nuget: FSharp.Stats"
 #r "nuget: Npgsql.FSharp"
-#r "nuget: MathNet.Numerics.FSharp"
 open FSharp.Data
 open System.IO
-open MathNet.Numerics.LinearAlgebra
 open Npgsql.FSharp
+open FSharp.Stats.ML
+open FSharp.Stats.ML.Unsupervised
+open FSharp.Stats.ML.Unsupervised.HierarchicalClustering
 
 //////////////////////////////////////////////////////////////////
 
@@ -17,16 +19,15 @@ let getWordEncoding (word:string) =
     |> Sql.execute (fun read -> 
         read.int64Array "encoder")
  
-let encoderToArray (encoder:int64[] list) : Vector<float> =
+let encoderToArray (encoder:int64[] list) : float[] =
     match encoder with
-    | [] -> vector [for i in 1..300 -> 0.0]
-    | _ -> vector (Array.map(fun x -> float(x)/1.0e6) (List.item 0 encoder))
+    | [] -> [|for i in 1..300 -> 0.0|]
+    | _ -> (Array.map(fun x -> float(x)/1.0e6) (List.item 0 encoder))
     //|> Array.map(fun x -> float(x)/1.0e6)
     //|> vector 
 
 let a = encoderToArray (getWordEncoding "cooling");;
 let b = encoderToArray (getWordEncoding "refrigeration");;
-let distance = (a-b).L2Norm() 
 
 let articlePage (pii:string) = match pii.Length with 
                                 | 17 -> HtmlDocument.Load("https://www.sciencedirect.com/science/article/pii/"+pii)
@@ -78,17 +79,20 @@ let getAllJournalKeywords (journalName:string, lastIssueId:int) =
     |> List.concat
     |> saveKeywords
 
-
-// A word counter for the csv file
-let wordCounter  (singleWords:seq<string>) (targetWord:string)  = 
-    let count = Seq.fold(fun (acc:int) (word:string)->  if word.Equals(targetWord) then acc+1 else acc+0) 0 singleWords
-    (targetWord,count)
-
 let getAllWords (filename:string) = 
     let csv = CsvFile.Load(filename, hasHeaders = true)
     csv.Rows
     |> Seq.map(fun (kw:CsvRow) -> kw.[0].Split(' '))
     |> Seq.concat
+
+let inputFile = "C:/Users/Fabrizio/Documents/Development/fsharp/dispata/dispata/test.csv"
+
+let ignoreWords:Set<string> = set ["and";"for";"nor";"but";"or";"yet";"so";"not";"as";"if";"such";"the";"in";"of";"to";"with";"on";"up";"down";"a";"an";"from";"by";"&"]
+
+// ------------------- A word counter for the csv file -----------------------//
+let wordCounter  (singleWords:seq<string>) (targetWord:string)  = 
+    let count = Seq.fold(fun (acc:int) (word:string)->  if word.Equals(targetWord) then acc+1 else acc+0) 0 singleWords
+    (targetWord,count)
 
 let storeWordCounts (words:seq<string*int>) = 
     let csvTuples = Seq.map(fun (x:string,y:int) -> x+","+string(y)) words
@@ -99,12 +103,34 @@ let countAllWords (filename:string) =
     Seq.distinct(allWords)
     |> Seq.map(wordCounter allWords)
     |> Seq.sortByDescending(fun (x,y)->y)
-// Still need to sort -> then use the embeddings for clustering
 
-let sortedWords = countAllWords  "C:/Users/Fabrizio/Documents/Development/fsharp/dispata/dispata/test.csv"
+let sortedWords = countAllWords  inputFile
 storeWordCounts sortedWords
+// -------------------------- The End ---------------------------------------//
 
-// NEXT: exclude conjunctions, prepositions etc.. from the count
-// Get the embeddings of each word
-// Clustering!
-// Cool plotting (words that are as big as their count, you know which kind)
+// Should get a dictionary first and a couple methods key-> value and value-> key for later plotting
+let getWordEncoderMapping (words:seq<string>) = 
+    Seq.map (fun (x:string) -> ((getWordEncoding >> encoderToArray) x,x)) words
+    |> Map.ofSeq
+   
+let EncoderMap = getWordEncoderMapping (getAllWords inputFile)
+
+//let b = encoderToArray (getWordEncoding "refrigeration");;
+let allEncoders (enMap:Map<float[],string>) : float[][] = Seq.toArray(Seq.cast(enMap.Keys))
+
+let encoders = allEncoders EncoderMap
+
+let rnd = new System.Random()
+let randomInitFactory : IterativeClustering.CentroidsFactory<float []> = 
+    IterativeClustering.randomCentroids<float []> rnd
+
+let kmeansResult = 
+    IterativeClustering.kmeans <| DistanceMetrics.euclidean <| randomInitFactory 
+    <|  encoders <| 30
+
+let wordGroups = 
+    let group, _ = Array.unzip (Array.map(fun (x:float[]) -> kmeansResult.Classifier x) encoders)
+    let word = Array.map(fun (x:float[]) -> EncoderMap[x]) encoders
+    Array.zip group word
+                        
+let (groupOne:array<int * string>) = Array.filter(fun (g,_) -> g.Equals 4 ) wordGroups   
